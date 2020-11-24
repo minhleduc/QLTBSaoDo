249e81062c2f16d1c2a2499fb86d586b1c9f8b16
Minh Le <53259342+minhleduc@users.noreply.github.com>
2020-11-24 21:18:36 +0700

    ThietBiInterface_v1

---
+++ QLTBSaoDo.ViewModels/Catalog/ThietBis/ThietBiUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLTBSaoDo.ViewModels.Catalog.ThietBis
+{
+    public class ThietBiUpdateRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Details { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
---
+++ QLTBSaoDo.ViewModels/Catalog/ThietBis/ThietBiViewModel.cs
@@ -0,0 +1,25 @@
+﻿using QLTBSaoDo.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLTBSaoDo.ViewModels.Catalog.ThietBis
+{
+    public class ThietBiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Details { get; set; }
+        public DateTime? DateCreated { get; set; }
+
+        public string Loai { get; set; }
+        public string DanhMuc { get; set; }
+
+        public virtual ICollection<ChiTietDonNhap> ChiTietDonNhap { get; set; }
+        public virtual ICollection<ThietBiDanhMuc> ThietBiDanhMuc { get; set; }
+        public virtual ICollection<ThietBiLoai> ThietBiLoai { get; set; }
+        public virtual ICollection<ThietBiPhong> ThietBiPhong { get; set; }
+    }
+}
